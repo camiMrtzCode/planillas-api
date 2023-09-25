e9aefc1e161d858614c5bef27772c794d68c2c33
Camilo Martinez <camilomrtz1998@gmail.com>
2023-09-25 18:51:42 -0500

    initial commit

---
+++ src/main/java/com/camimrtzcode/restapi/calculototalplanillas/CalculoTotalPlanillasApplication.java
@@ -0,0 +1,13 @@
+package com.camimrtzcode.restapi.calculototalplanillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoTotalPlanillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoTotalPlanillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camimrtzcode/restapi/calculototalplanillas/CalculoTotalPlanillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camimrtzcode.restapi.calculototalplanillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoTotalPlanillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
